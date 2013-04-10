463c6c01cad9c9b4315bfe084a8c4dcb63a7f80a
Qin Shao Hui <qshadun@gmail.com>
2013-04-10 13:50:18 +0800

    project euler 1 ~ 3

---
+++ src/main/scala/projectEuler/Problem1.scala
@@ -0,0 +1,34 @@
+package projectEuler
+
+/**
+ * Multiples of 3 and 5
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+object Problem1 {
+
+  // Wrong, this will calculate all numbers only dividable by 3 or 5
+  def findAllNumbers(max: Int): List[Int] = {
+    val start = List(3, 5)
+    def f(l: List[Int]): Stream[List[Int]] = {
+      val n = for (x <- l) yield List(x * 3, x * 5)
+      l #:: f(n.flatten)
+    }
+    f(start).takeWhile(ls => ls.exists(_ <= max)).toList.flatten.distinct.sort(_ < _).takeWhile(_ <= max)
+  }
+  
+  def findAllNumbers1(max: Int): List[Int] = {
+    (1 until max).filter(x => x % 3 == 0 || x % 5 == 0).toList
+  }
+  
+  def sumMultipleoOf3And5(max: Int): Int = {
+    List.range(3, max, 3).sum + List.range(5, max, 5).sum - List.range(15, max, 15).sum
+  }
+  def main(args: Array[String]): Unit = {
+    val allLessThan1000 = findAllNumbers1(1000) 
+    println(allLessThan1000)
+    println(allLessThan1000.sum)
+    println(sumMultipleoOf3And5(1000))
+  }
+
+}
---
+++ src/main/scala/projectEuler/Problem3.scala
@@ -0,0 +1,30 @@
+package projectEuler
+
+/**
+ * Largest prime factor
+Problem 3
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+ */
+object Problem3 {
+
+  def isPrime(n: Long) = {
+    (2L to Math.sqrt(n).toLong).forall(n % _ != 0) 
+  }
+  
+  def findLargestFactor(n: Long) = {
+    lazy val naturals: Stream[Int] = Stream.cons(1, naturals.map(_ + 1))
+    var theNum = n
+    naturals.drop(1).dropWhile(n => {while(theNum % n == 0) {theNum /= n}; theNum > 1}).head
+  }
+  
+  def main(args: Array[String]): Unit = {
+    val n = 600851475143L;
+//    val primes = (2L to Math.sqrt(n).toLong).filter(isPrime)
+//    println((primes).filter(n % _ == 0).last)
+//    
+    println(findLargestFactor(n))
+  }
+
+}
