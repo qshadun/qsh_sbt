5b60176a914017ab58c3b00117e165049fe9139d
Qin Shao Hui <qshadun@gmail.com>
2013-04-10 17:14:12 +0800

    aa

--- src/main/scala/projectEuler/Problem5.scala
+++ src/main/scala/projectEuler/Problem5.scala
@@ -26,26 +26,37 @@ object Problem5 {
     }
     result
   }
+  
+  def gcd(x: Long, y: Long): Long = {
+    if (x % y == 0) y
+    else gcd(y, x % y)
+  }
+  
   def main(args: Array[String]): Unit = {
+    //Brute force
 //    def isSuitable(n: Long) = (2 to 20).forall(n % _ == 0)
 //    val start = 2520L* 11 * 13 * 17 * 19
 //    def candidates = Stream.iterate(2520L* 11 * 13 * 17 * 19){_ + 1}
 //    println(candidates.dropWhile(!isSuitable(_)).head)
     
-    val allFactors = (2 to 20).map(findFactors).map(groupFactors) 
-    println(allFactors)
-    val allNeededFactors = allFactors.reduceLeft {(acc, m) =>
-      m.keys.foreach {x =>
-        if (acc.contains(x)) acc(x) = Math.max(acc(x), m(x))
-        else acc(x) = m(x)
-      }
-      acc
-    }
-    println(allNeededFactors)
-    val result = allNeededFactors.keys.foldLeft(BigInt(1)) {(acc, x) =>
-      acc * Math.pow(x, allNeededFactors(x)).toInt
-    }
-    println(result)
+    // Find all factors
+//    val allFactors = (2 to 20).map(findFactors).map(groupFactors) 
+//    println(allFactors)
+//    val allNeededFactors = allFactors.reduceLeft {(acc, m) =>
+//      m.keys.foreach {x =>
+//        if (acc.contains(x)) acc(x) = Math.max(acc(x), m(x))
+//        else acc(x) = m(x)
+//      }
+//      acc
+//    }
+//    println(allNeededFactors)
+//    val result = allNeededFactors.keys.foldLeft(BigInt(1)) {(acc, x) =>
+//      acc * Math.pow(x, allNeededFactors(x)).toInt
+//    }
+//    println(result)
+    
+    // multiply 1~20 and devide gcds on the way 
+    println((2L to 20L).reduce((x, y) => x * y / gcd(x, y)))
   }
 
 }
