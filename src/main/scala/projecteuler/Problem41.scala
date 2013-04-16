edf1fbd20192048aed4f93490dc17f3d9c9a08cb
Qin Shao Hui <qshadun@gmail.com>
2013-04-16 14:58:50 +0800

    aa

--- src/main/scala/projecteuler/Problem41.scala
+++ src/main/scala/projecteuler/Problem41.scala
@@ -10,20 +10,21 @@ What is the largest n-digit pandigital prime that exists?
 object Problem41 {
   def main(args: Array[String]): Unit = {
     def isPrime(n: Int) = n == 2 || n > 2 && (2 to Math.sqrt(n).toInt).forall(n % _ != 0)
-    def combines(n: Int): List[Int] = {
-      require(n < 10 && n > 0)
-      val digits = (1 to n).toList.reverse
-      def combineR(used: List[Int], pos: Int, remain: List[Int]): List[Int] = 
-        if (pos == n) List(used.reverse.mkString.toInt)
-        else remain.map{x =>
-          combineR(x :: used, pos + 1, remain - x)
-        }.flatten
-      combineR(Nil, 0, digits)
-    } 
+//    def combines(n: Int): List[Int] = {
+//      require(n < 10 && n > 0)
+//      val digits = (1 to n).toList.reverse
+//      def combineR(used: List[Int], pos: Int, remain: List[Int]): List[Int] = 
+//        if (pos == n) List(used.reverse.mkString.toInt)
+//        else remain.map{x =>
+//          combineR(x :: used, pos + 1, remain - x)
+//        }.flatten
+//      combineR(Nil, 0, digits)
+//    } 
     def find(start: Int): Int = {
-      val num = combines(start).find(isPrime).getOrElse(0)
+      val num = (start to 1 by -1).permutations.map(_.mkString.toInt).find(isPrime).getOrElse(0)
       if (num > 0) num else find(start - 1)
     }
-    println(find(9))
+    // only 1, 4 and 7 need to be considered, because n(n+1)/2 will be dividable by 3 otherwise
+    println(find(7))
   }
 }
