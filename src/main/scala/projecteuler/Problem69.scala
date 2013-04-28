dd2450208ee4e2c68613c945eb356b7a526b3822
Qin Shao Hui <qshadun@gmail.com>
2013-04-29 06:52:25 +0800

    aa

--- src/main/scala/projecteuler/Problem69.scala
+++ src/main/scala/projecteuler/Problem69.scala
@@ -1,33 +1,50 @@
 package projecteuler
+import scala.collection.mutable.ArrayBuffer
 object Problem69 {
   def main(args: Array[String]) = {
-    def sieve = Sieve(1000001)
-    def isPrime = sieve.isPrime _
+    def sieve = Sieve(1000000)
+    def isPrime(n: Int) = if (n < 1000000) sieve.isPrime(n) else (2 to math.sqrt(n).toInt).forall(n % _ != 0)
     val primes = sieve.getPrimes
-    def factorials(n: Int) = {
-      def recur(remain: Int, fs: Seq[Int]): List[Int] = {
-        if (remain == 1) Nil
-        else {
-          var r = remain
-          if (r % fs.head == 0) {
-            while (r % fs.head == 0) r = r / fs.head
-            if (isPrime(r)) List(fs.head, r)
-            else fs.head :: recur(r, fs.tail)
-          }
-          else recur(r, fs.tail)
-        }
-      }
-      recur(n, primes.takeWhile(_ <= n / 2))
-    }
-    println(factorials(1000000))
-    def phi(n: Int): Int = 
-      if (isPrime(n)) n -1
-      else 
-        factorials(n).foldLeft(Rational(n, 1)){(acc, x) =>
-          acc * Rational(x - 1, x)
-        }.normalize.n.toInt
     
-    println(primes.scanLeft((1,1))((acc, x)=> (acc._1 * x, x)).takeWhile(_._1 <= 1000000).last)
+    def primeFactors(n: Int) = {
+      var pfs = ArrayBuffer.empty[Int]
+      var i = 2
+      var remain = n
+      while(remain != 1 && i < n) {
+        if (remain % i == 0) {
+          pfs.append(i)
+          while(remain % i == 0) remain = remain / i
+        }
+        i = i + 1
+      }
+      pfs.toList
+    }
+    
+    def phi(n: Int): Int = 
+      if (n == 1) 1
+      else 
+        if (isPrime(n)) n -1
+        else {
+          val fs = primeFactors(n)
+          val norm = fs.foldLeft(BigInt(1)){case (acc, x) => acc * (x - 1)}
+          val denorm = fs.foldLeft(BigInt(1))(_ * _)
+          (n * norm / denorm).toInt
+        }
+    def subCalc(ls: Seq[Int]) = 
+      ls.map(n => (n, phi(n))).filter{case (n, p) => n.toString.sorted == p.toString.sorted}
+    val ab = ArrayBuffer.empty[(Int, Int)]
+    for (i <- 1 to 1000) {
+      val lb = if (i == 1) 2 else (i - 1) * 10000 
+      val ub = i * 10000
+      val sub = subCalc(lb until ub)
+      if (!sub.isEmpty) {
+        val min = sub.minBy{case (n, p) => n.toDouble / p}
+        ab.append(min)
+        println(min)
+      }
+    }
+    println("final result: " + ab.minBy{case (n, p) => n.toDouble / p})
+//    println(primes.scanLeft((1,1))((acc, x)=> (acc._1 * x, x)).takeWhile(_._1 <= 1000000).last)
 //   println((3 to 1000000).foldLeft((2, 2d)){(acc,n) => 
 //     val r = (n.toDouble / phi(n))
 //     if (r > acc._2) (n, r) else acc
