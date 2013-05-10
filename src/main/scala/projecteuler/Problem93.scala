8de290dbfbc2ce6c3a0ff6afa11ea97e1a0d8513
Qin Shao Hui <qshadun@gmail.com>
2013-05-10 15:37:11 +0800

    93

--- src/main/scala/projecteuler/Problem93.scala
+++ src/main/scala/projecteuler/Problem93.scala
@@ -1,5 +1,5 @@
 package projecteuler
-
+//Arithmetic expressions
 object Problem93 {
   def eval(a: Int, b: Int, op: String) =
     op match {
@@ -12,19 +12,18 @@ object Problem93 {
   def possibleResult(ls: Seq[Int], ops: Seq[String]): Seq[Int] = ops match {
     case Nil => ls
     case _ =>
-      val r1 = try {
-        possibleResult(eval(ls(0), ls(1), ops.head) +: ls.tail.tail, ops.tail)
-      } catch {
-        case _: Throwable => Nil
-      }
-      val r2 = possibleResult(ls.tail, ops.tail).map {x =>
-        try {
-          eval(ls.head, x, ops.head)
-        } catch {
-          case _: Throwable => Int.MinValue
-        }
-      }
-      (r1 ++ r2.filterNot(_ == Int.MinValue)).distinct
+      val r = (1 to ls.size - 1).map { i =>
+        possibleResult(ls.take(i), ops.take(i - 1)).map {n1 =>
+          possibleResult(ls.drop(i), ops.drop(i)).map {n2 =>
+            try {
+              eval(n1, n2, ops(i-1))
+            } catch {
+              case _: Throwable => Int.MinValue
+            }
+          }
+        }.flatten
+      }.flatten
+      r.filterNot(_ == Int.MinValue).distinct
   }
   val ops = List("+", "-", "*", "/")
   val opsCombo = for (
@@ -39,11 +38,12 @@ object Problem93 {
     ) yield possibleResult(ns, os)
     val pr = r.flatten.toList.filter(_ > 0).distinct.sorted
     val n = (0 until pr.length).find(i => pr(i) != i + 1)
-
     (ns.mkString, if (n.isDefined) n.get else pr.length)
   }
   def main(args: Array[String]): Unit = {
-    val r = (0 to 9).combinations(4).map(consecutiveNum).maxBy(_._2)
-    println(r)
+    val r = (0 to 9).combinations(4).map(consecutiveNum).toList
+    val max = r.maxBy(_._2)._2
+    val result = r.filter(_._2 == max)
+    println(result)
   }
 }
