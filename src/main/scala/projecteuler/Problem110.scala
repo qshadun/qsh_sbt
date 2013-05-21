08f28b54727238cfd9238ca3bad7d6fbc355b61d
Qin Shao Hui <qshadun@gmail.com>
2013-05-21 15:48:31 +0800

    110

--- src/main/scala/projecteuler/Problem110.scala
+++ src/main/scala/projecteuler/Problem110.scala
@@ -5,7 +5,7 @@ p1, p2, ..., pt are prime factors of n.
 So the base solution for this problem is the product of the first 15 primes (3 ^ 15 > 8000000)
 Then I tried to decrease the number of prime factors to 14. 
 Start from a1 = a2 = ... = a14 = 1, increase the exponents heuristically and find the smallest number which has more than 4000000 solutions.
-Then find the smallest number for 13 prime factors until I can find a smaller number.
+Then find the smallest number for 13 prime factors until I cannot find a smaller number.
  */
 object Problem110 {
   val sieve = Sieve(100)
@@ -16,7 +16,7 @@ object Problem110 {
     import scala.collection.mutable
     val m = mutable.Map.empty[Int, Int]
     val ps = primes.take(n)
-    ps.tail.foreach { p =>
+    ps.tail.foreach { p => // 3 > 2, 5 > 2, 7 > 2 * 3
       var i = 1
       while (ps.take(i).product < p) i = i + 1
       m(p) = i
@@ -29,11 +29,10 @@ object Problem110 {
         else {
           val s1 = {
             val newExps = exps.take(i) ::: exps(i) + 1 :: exps.drop(i + 1)
-            if (i < exps.length - 1)
-              recur(newExps, i + 1)
-            else recur(newExps, 0)
+            recur(newExps, (i + 1) % n)
           }
-          val s2 = recur(exps.take(i).map(_ + 1) ::: exps.drop(i), i)
+          val smalls = m(primes(i))
+          val s2 = recur(exps.take(smalls).map(_ + 1) ::: exps.drop(smalls), i)
           if (toNumber(s1) < toNumber(s2)) s1 else s2
         }
       }
@@ -54,6 +53,7 @@ object Problem110 {
     result
   }
   def main(args: Array[String]): Unit = {
-    println(solve(4000000))
+    val bound = if (args.length > 0) args(0).toInt else 4000000
+    println(solve(bound))
   }
 }
