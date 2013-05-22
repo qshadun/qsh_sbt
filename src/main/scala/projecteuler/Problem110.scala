412031c73e669bd38587a7f933c87389a6fac907
Qin Shao Hui <qshadun@gmail.com>
2013-05-22 11:10:48 +0800

    110

--- src/main/scala/projecteuler/Problem110.scala
+++ src/main/scala/projecteuler/Problem110.scala
@@ -1,11 +1,11 @@
 package projecteuler
 /**
-If n = (p1^a1)(p2^a2)...(pt^at), a(n) = ((2 * a1 + 1)(2 * a2 + 1) ... (2 * at + 1) + 1)/2. (http://oeis.org/A018892)
-p1, p2, ..., pt are prime factors of n.
-So the base solution for this problem is the product of the first 15 primes (3 ^ 15 > 8000000)
-Then I tried to decrease the number of prime factors to 14. 
-Start from a1 = a2 = ... = a14 = 1, increase the exponents heuristically and find the smallest number which has more than 4000000 solutions.
-Then find the smallest number for 13 prime factors until I cannot find a smaller number.
+ * If n = (p1^a1)(p2^a2)...(pt^at), a(n) = ((2 * a1 + 1)(2 * a2 + 1) ... (2 * at + 1) + 1)/2. (http://oeis.org/A018892)
+ * p1, p2, ..., pt are prime factors of n.
+ * So the base solution for this problem is the product of the first 15 primes (3 ^ 15 > 8000000)
+ * Then I tried to decrease the number of prime factors to 14.
+ * Start from a1 = a2 = ... = a14 = 1, increase the exponents heuristically and find the smallest number which has more than 4000000 solutions.
+ * Then find the smallest number for 13 prime factors until I cannot find a smaller number.
  */
 object Problem110 {
   val sieve = Sieve(100)
@@ -27,10 +27,7 @@ object Problem110 {
       else {
         if (i == 0) recur(exps.head + 1 :: exps.tail, 1)
         else {
-          val s1 = {
-            val newExps = exps.take(i) ::: exps(i) + 1 :: exps.drop(i + 1)
-            recur(newExps, (i + 1) % n)
-          }
+          val s1 = recur(exps.take(i) ::: exps(i) + 1 :: exps.drop(i + 1), (i + 1) % n)
           val smalls = m(primes(i))
           val s2 = recur(exps.take(smalls).map(_ + 1) ::: exps.drop(smalls), i)
           if (toNumber(s1) < toNumber(s2)) s1 else s2
